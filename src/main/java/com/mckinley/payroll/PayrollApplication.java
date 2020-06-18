59374f35a402553285ddd3e6d39fe7e6f7a422b5
Gary McKinley <gary@garymckinley.com>
2020-06-18 08:58:32 -0500

    Removed logging

--- src/main/java/com/mckinley/payroll/PayrollApplication.java
+++ src/main/java/com/mckinley/payroll/PayrollApplication.java
@@ -1,7 +1,5 @@
 package com.mckinley.payroll;
 
-import java.util.logging.Logger;
-
 //	Payroll MicroServices
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayrollApplication {
 
-	private final static Logger logger = Logger.getLogger("Payroll Application");
 	public static void main(String[] args) 	{		
-		logger.info("starting");
 		SpringApplication.run(PayrollApplication.class, args);
 	}
 }
