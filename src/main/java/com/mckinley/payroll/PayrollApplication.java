4417e6f8bbfc659c96f6b5b9535fce273458fa34
Gary McKinley <gary@garymckinley.com>
2020-06-19 08:32:27 -0500

    Turn OFF Spring Banner

--- src/main/java/com/mckinley/payroll/PayrollApplication.java
+++ src/main/java/com/mckinley/payroll/PayrollApplication.java
@@ -1,5 +1,6 @@
 package com.mckinley.payroll;
 
+import org.springframework.boot.Banner;
 //	Payroll MicroServices
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayrollApplication {
 
-	public static void main(String[] args) 	{		
-		SpringApplication.run(PayrollApplication.class, args);
+	public static void main(String[] args) 	{
+		
+		SpringApplication springApp = new SpringApplication(PayrollApplication.class);
+		
+		springApp.setBannerMode(Banner.Mode.OFF);
+		
+		springApp.run(args);
+		
 	}
 }
